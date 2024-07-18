0ee56737c628f96f3250c0732b4c756defbfbd48
Shwetagupta2710 <shwetaguptas2710@gmail.com>
2024-07-19 01:43:46 +0530

    change

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ class Face_Recognition_System:
         self.root.geometry("1530x790+0+0")
         self.root.title("Face Recognition System")
 
+        img=Image.open(r"C:\Users\shwet\OneDrive\Desktop\Face-Recognition-Attendance-System\Images\black.jpg")
+        img=img.resize((500,130),Image.ANTIALIAS)
+        self.photoimg=ImageTk.PhotoImage(img)
+
+        f_lbl=Label(self.root,image=self.photoimg)
+        f_lbl.place(x=0,y=0,width=500,height=130)
 
 
 
 
-        if __name__ == '__main__':
-            root = Tk()
-            obj = Face_Recognition_System(root)
-            root.mainloop()
+if __name__ == '__main__':
+    root = Tk()
+    obj = Face_Recognition_System(root)
+    root.mainloop()
+
