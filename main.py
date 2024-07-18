f3d0d29e0c06881a09538188d49832585541a423
Shwetagupta2710 <shwetaguptas2710@gmail.com>
2024-07-19 02:11:25 +0530

    img

--- main.py
+++ main.py
@@ -6,21 +6,29 @@ from PIL import Image,ImageTk
 class Face_Recognition_System:
     def __init__(self,root):
         self.root=root
-        self.root.geometry("1530x790+0+0")
+        self.root.geometry("1366x768+0+0")
         self.root.title("Face Recognition System")
 
         img=Image.open(r"C:\Users\shwet\OneDrive\Desktop\Face-Recognition-Attendance-System\Images\black.jpg")
-        img=img.resize((500,130),Image.ANTIALIAS)
+        img=img.resize((1366,128),Image.Resampling.LANCZOS)
         self.photoimg=ImageTk.PhotoImage(img)
 
         f_lbl=Label(self.root,image=self.photoimg)
-        f_lbl.place(x=0,y=0,width=500,height=130)
+        f_lbl.place(x=0,y=0,width=1366,height=138)
+
+        title_lbl = Label(f_lbl, text = "ATTENDANCE SYSTEM BASED ON FACE RECOGNITION", font=("times new roman", 30, "bold"),bg="white",fg="purple")
+        title_lbl.place(x=25,y=25,width = 1300,height = 90)
+
+        #Background image.
+        img2 = Image.open(r"C:\Users\shwet\OneDrive\Desktop\Face-Recognition-Attendance-System\Images\background.jpeg")
+        img2 = img2.resize((1366,640),Image.Resampling.LANCZOS) 
+        self.photoimg2 = ImageTk.PhotoImage(img2)
 
 
 
 
-if __name__ == '__main__':
-    root = Tk()
+
+if __name__=="__main__":
+    root=Tk()
     obj = Face_Recognition_System(root)
     root.mainloop()
-
