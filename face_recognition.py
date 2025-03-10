2c5985782e77be55010ef0b62b3e8cb0c1339b76
Shwetagupta2710 <shwetaguptas2710@gmail.com>
2025-03-11 00:20:16 +0530

    added

--- face_recognition.py
+++ face_recognition.py
@@ -77,13 +77,35 @@ class Face_Recognition:
                 r = "+".join(r)
 
                 if confidence>77:
-                    cv2.putText(img, f"Name:{n}", (x,y-55), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255), 
+                    cv2.putText(img, f"Roll:{r}", (x,y-55), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255), 3)
+                    cv2.putText(img, f"Name:{n}", (x,y-30), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255), 3)
+                    cv2.putText(img, f"Department:{d}", (x,y-5), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255), 3)
+                else:
+                    cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 3)
+                    cv2.putText(img, "Unknown Face", (x,y-5), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255), 3)
 
+                coord=[x,y,w,y]
+            return coord
+        
+        def recognize(img, clf, faceCascade):
+            coord = draw_boundary(img, faceCascade, 1.1, 10, (255,25,255), "Face", clf)
+            return img
+        
+        faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        clf = cv2.face.LBPHFaceRecognizer_create()
+        clf.read("classifier.xml")
 
-                
- 
+        video_cap = cv2.VideoCapture(0)
 
+        while True:
+            ret, img = video_cap.read()
+            img = recognize(img, clf, faceCascade)
+            cv2.imshow("Welcome To Face Recognition", img)
 
+            if cv2.waitKey(1) == 13:
+                break
+        video_cap.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
