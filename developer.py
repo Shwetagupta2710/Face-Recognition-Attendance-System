f4e8be44f13adeb338ab45779913168dac9bd61f
Shwetagupta2710 <shwetaguptas2710@gmail.com>
2025-03-12 00:52:18 +0530

    new changes

--- developer.py
+++ developer.py
@@ -1,6 +1,4 @@
-from cProfile import label
-from cgitb import text
-from email.mime import image
+
 from tkinter import *
 from tkinter import ttk
 from traceback import format_exc
@@ -21,7 +19,37 @@ class Developer:
         desired_width = full_screen_width // 3
         lower_img_height=self.root.winfo_screenheight()-298
 
+        title_lbl = Label(self.root, text="DEVELOPER",
+                          font=("times new roman", 35, "bold"), bg="white", fg="blue")
+        title_lbl.place(x=0, y=0, width=full_screen_width, height=45)
 
+        back_btn = Button(title_lbl, text="Back", command=self.close_window,width=21,
+            font=("times new roman", 10, "bold"),
+            bg="blue",
+            fg="white",)
+        back_btn.place(x=30,y=5)
+        
+        img_top = Image.open("images/dev.webp")
+        img_top = img_top.resize((1530, 730), Image.LANCZOS)
+        self.photoimage_top = ImageTk.PhotoImage(img_top)
+
+        top_lbl = Label(self.root, image=self.photoimage_top)
+        top_lbl.place(x=0, y=55, width=1530, height=730)
+        #frame
+        main_frame=Frame(top_lbl, bd=2, bg="white")
+        main_frame.place(x=1000, y=0, width=500, height=600)
+
+        img_top = Image.open("images/dev.webp")
+        img_top = img_top.resize((1530, 730), Image.LANCZOS)
+        self.photoimage_top = ImageTk.PhotoImage(img_top)
+
+        top_lbl = Label(self.root, image=self.photoimage_top)
+        top_lbl.place(x=0, y=55, width=1530, height=730)
+
+
+
+    def close_window(self):
+        self.root.destroy()
 
 if __name__ == "__main__":
     root = Tk()
--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 from tkinter import*
-from tkinter import ttk
 from PIL import Image, ImageTk
 from tkinter import messagebox
 import mysql.connector
